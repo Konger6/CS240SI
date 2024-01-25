8f1fd25f8f0dceef253d9170255d025060cffd17
Konger6 <112506635+Konger6@users.noreply.github.com>
2024-01-24 23:28:46 -0500

    Create cheap_meals.c

---
+++ Structs /cheap_meals.c
@@ -0,0 +1,47 @@
+/* cheap_meals.c - file to struct */
+/* Made by Sophia Konger */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define MAX_FOOD_ITEMS[15];
+
+/*
+ * INSTRUCTIONS:
+ * Create a struct, foodItem_data_t, of typedef, foodItem, with variables: int calories, int fat
+ * int carbs, float protein, float price
+ */
+
+FoodItem g_data_arr[MAX_FOOD_ITEMS];
+int g_num_items = 0;
+
+/*---------------------------------------------------------------
+ * reads data from a file, macros.txt, and stores it in a global Food array
+ */--------------------------------------------------------------
+
+int read_data(char *filename) {
+    return 1;
+}
+
+/*---------------------------------------------------------------
+ * records all the entries in order of cost, from cheapest to most expensive
+ *---------------------------------------------------------------
+ */
+
+void filter_by_cost() {
+  
+}
+
+/*---------------------------------------------------------------
+ * writes all the entries in the exact same way it shows in original file, but filtered by cost
+ *---------------------------------------------------------------
+ */
+
+int write_data(char *filename) {
+}
+
+int main() {
+}
+ 
