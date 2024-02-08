574fdaf2073e8aec3c38fa84f23f22003c0d3382
Konger6 <112506635+Konger6@users.noreply.github.com>
2024-02-08 12:07:39 -0500

    Create pointer_example.c

---
+++ Pointers/pointer_example.c
@@ -0,0 +1,26 @@
+// Instructions for Pointers in C
+
+#include <stdio.h>
+
+void swap (int *a, int *b); // function prototype
+
+/* Task 1: Write a function named 'swap' that takes two pointers to integers as parameters
+   and swaps the values of the integers they point to. */
+
+
+/* Inside Main Method
+  Create a main method, and create an example usage of the swap fcn
+  and the correct output */
+
+/* Task 2: Understand pointer dereferencing.  Inside Main Method
+   - Create a pointer 'p' and assign it the address of an integer variable 'x' that has a specific value.
+   - Print the value of 'x' by dereferencing 'p'.
+   - Write what the output would be */
+
+
+/* Task 3: Explore the relationship between arrays and pointers. Inside Main Method
+   - Create an integer array and a pointer.
+   - Assign the address of the first element of the array to the pointer.
+   - Use the pointer to traverse the array and print each element.
+   - Write the expected output
+   */
